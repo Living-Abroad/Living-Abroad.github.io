101be85a48478f50a985703a32cef4660e068d0b
Beccacheesecake17 <59771585+Beccacheesecake17@users.noreply.github.com>
2020-02-25 10:57:47 +0000

    Fixed button position permenantly (I hope)

--- Interactive.stuff.js
+++ Interactive.stuff.js
@@ -2,3 +2,9 @@ showModal = function(name) {
     modal = document.getElementById(name);
     modal.style.display = "block";
   };
+
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
